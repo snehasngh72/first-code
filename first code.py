efcb3648559786a1a27a201c5854839850e4a0d1
Sneha singh <167342268+snehasngh72@users.noreply.github.com>
2024-04-24 08:44:42 +0530

    Add files via upload

---
+++ first code.py
@@ -0,0 +1,4 @@
+print("HELLO WORLD")
+print("Me SNEHA \n Here Introducing Myself")
+print("My Hobbies are \'sketching\' and \'coding\'")
+print("I am Science Student \n and currently started learning python ")
